405b115ec3be145e3cd8a97b357759cbc5ae7a9f
EmilianoAdelarBade <58085172+emigrox@users.noreply.github.com>
2022-05-29 22:46:42 -0300

    todo listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticuloComponent } from './components/articulo/articulo.component';
+import { PresentacionComponent } from './components/presentacion/presentacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticuloComponent,
+    PresentacionComponent
   ],
   imports: [
     BrowserModule,
